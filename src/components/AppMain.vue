66c543140374df270ff1ca1d110dd53b937659cb
quintusdev <126263354+quintusdev@users.noreply.github.com>
2023-08-29 15:15:03 +0200

    [FEATURES] inserimento Loader e img in main

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,12 +1,16 @@
 <script>
+import AppLoader from './AppLoader.vue';
+import axios from 'axios';
+
 export default {
   name: 'AppMain',
+  components: { AppLoader },
   data() {
     return {
       baseUrl: 'http://localhost:8000',
       posts: [],
       loading: true
-    }
+    };
   },
   created() {
     this.getPosts();
@@ -20,16 +24,29 @@ export default {
         this.posts = response.data.results.data;
         /* imposto il loading a false */
         this.loading = false;
-      })
+      });
     }
-  }
+  },
 }
 </script>
 
 <template>
   <div class="row">
     <div class="col-12">
-      <h1 class="text-center">I nostri Progetti</h1>
+      <h1 class="text-center">BOOLFOLIO</h1>
+      <p v-if="!loading" class="text-center"><strong>{{ posts }}</strong></p>
+    </div>
+    <AppLoader v-if="loading" />
+    <div v-else class="container">
+      <div class="row">
+        <div class="col-12" v-for="post in posts" :key="post.id">
+          <div class="card">
+            <div class="card-image-top">
+              <img :src="`${baseUrl}/${post.img}`" class="img-fluid" alt="Immagine del post">
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
